d6cc714bfcb1ef04751c9c7ae47ef1cc3f5f3498
Matheus de Assis <assis4002@gmail.com>
2020-10-27 23:53:08 -0300

    Entities implemented

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace csharp_Exercise03_Orders.Entities
+{
+    class OrderItem
+    {
+        public int Quantify { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int quantify, double price)
+        {
+            Quantify = quantify;
+            Price = price;
+        }
+
+        public double SubTotal()
+        {
+            return Quantify * Price;
+        }
+
+        public override string ToString()
+        {
+            return Product.Name
+                + ", $" + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantify: " + Quantify
+                + ", Subtotal:" + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace csharp_Exercise03_Orders
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
